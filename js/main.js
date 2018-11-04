059aa63574b844e6d33788e9b153535d052aa968
Nyabongo E.T <nyabongoedgar@gmail.com>
2018-11-04 20:32:31 +0300

    adds dropdown menu

--- js/main.js
+++ js/main.js
@@ -52,3 +52,8 @@ function change_location_admin(){
 
 
 }
+// displaying a specific div
+function display(){
+    var x =  document.getElementById("nav-ul");
+    x.classList.toggle("height-adj");
+}
