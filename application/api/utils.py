f93d28ca489b1c5ce777805ab5af08d36898ed12
Nyabongo E.T <nyabongoedgar@gmail.com>
2018-11-21 11:39:32 +0300

    adds some validation methods in utils.py

--- application/api/utils.py
+++ application/api/utils.py
@@ -1,6 +1,63 @@
-# class Helpers:
-#     staticmethod
-#     def check_required_fields(args,expected):
-#         for item in args:
-#             if item not in expected:
-#                 error.append({'message':item +' missing'}) 
+class Helpers:
+
+    @staticmethod
+    def search(items_list,item_id,item_key): 
+        for item in items_list:
+            if item[item_key] == int(item_id):
+                return item
+          
+
+    @staticmethod
+    def gen_id(items_list,item_key):
+        if len(items_list) == 0:
+            item_id  = 1
+            return item_id
+        else:
+            
+            last_dict = items_list[len(items_list)-1]
+            id = int(last_dict[item_key])
+            item_id = int(id + 1)
+            return item_id
+
+    @staticmethod
+    def gen_price(weight):
+        price = ""
+        if weight <= 0 :
+            price = 'weight should not be zero'
+        if (weight > 0 and weight <= 20):
+            price = 5000
+        if (weight > 20 and weight <= 50):
+            price = 10000
+        if (weight > 50 and weight <= 100):
+            price = 15000
+        if (weight > 100 and weight <= 1000):
+            price = 20000
+        if weight > 1000:
+            price = 'Weight exceeded'
+        return price
+
+
+    staticmethod
+    def validate_strings(args):
+        for i in args:
+            if not isinstance(i,str) or i.isspace():
+                return 'Data provided should be a string and should not be a space'
+
+    staticmethod
+    def validate_integer(args):
+        for i in args:
+            if not isinstance(i,int) or i < 0:
+                return 'Data provided should be an integer and should not be a positive number'
+    @staticmethod    
+    def modify_status(mylist,key,new_entry,parcelId):
+	    for i in mylist:
+		    if i[key] == parcelId:
+			    i['status'] = new_entry
+	    return i
+
+    @staticmethod
+    def check_if_exists(args,myList):
+        for item in myList:
+            if ( item['parcel_name'] == args['parcel_name'] and item['parcel_description'] == args['parcel_description']
+                 and item ['parcel_weight'] == args['parcel_weight']  and item['parcel_destination'] == args['parcel_destination'] ):
+                return 'Parcel order delivery already placed, please make a unique order'
