bbe145690da1d88fdf31ab4c2b3d3e576cda35e6
Nyabongo E.T <nyabongoedgar@gmail.com>
2018-11-22 18:10:03 +0300

    separate user views from parcel views

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, Blueprint
 app = Flask(__name__)
 
-from application.api.views import mod
-app.register_blueprint(api.views.mod)
+from application.api.views.parcel_views import parcel
+app.register_blueprint(api.views.parcel_views.parcel)
+
+from application.api.views.user_views import user_blueprint
+app.register_blueprint(api.views.user_views.user_blueprint)
 
 app.config.from_object('config.BaseConfig')
+
+
 
 
--- application/api/views/parcel_views.py
+++ application/api/views/parcel_views.py
@@ -7,62 +7,22 @@ from db import DatabaseConnection
 from application.api.models.user import User
 from application.api.models.parcels import Parcel
 from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
+    jwt_required, get_jwt_identity
 )
 
-jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = '\x01\n=:\x87\xe1\x02\xca\x81\x8b\x0c\xe4Y=\x87\xb7\xa8\x89.<\x95\x90\xbb\x06'
  
-mod = Blueprint('Parcel',__name__, url_prefix='/api/v2/')
+ 
+parcel = Blueprint('Parcel',__name__, url_prefix='/api/v2/')
 
-user_object = User()
+
 parcel_object = Parcel()
 conn_object = DatabaseConnection()
  
-@mod.route('/')
+@parcel.route('/')
 def index():
-    return jsonify({'message':'Welcome to the SendIT application'}),200
- 
-@mod.route('/auth/signup', methods = ['POST']) 
-def register_user():
-    """ This function registers a user by using his username,password and email """
-    data = request.get_json()
-    email = data.get('email')
-    username = data.get('username')
-    password = data.get('password') 
-    user = user_object.user(username)
-    if not password or password.isspace():
-        return jsonify({'message': 'Password field can not be left empty.'}), 400
-    if not username or username.isspace():
-        return jsonify({'message': 'Username field can not be empty.'}), 400
-    if not email or email.isspace():
-        return jsonify({'message': 'Email field can not be empty.'}), 400
-    elif not re.match(r"[^@.]+@[A-Za-z]+\.[a-z]+", email):
-        return jsonify({'message': 'Enter a valid email address.'}), 400  
-    if user is not None:
-        return jsonify({'message':'Username already exists'}),400 
-    hashed_password = generate_password_hash(password, method='sha256')
-    user_object.register_user(username,email,hashed_password)
-    return jsonify({'message':'User registered successfully'}),201
-    
-   
+    return jsonify({'message':'Welcome to the SendIT application'}),200   
 
-@mod.route("/auth/login", methods=['POST'])
-def login():
-    """ This function signs in  user by using his username and password """ 
-    data = request.get_json() 
-    if not data or not data.get('username') or not data.get('password'):
-        return jsonify({'message':'No data has been sent'}),400
-    user = user_object.user(data.get('username'))
-    if not user:
-        return jsonify({'message':'Verification of credentials failed !'}),401
-    if check_password_hash(user['password'],data['password']):
-        token = create_access_token(identity=user['user_id'])
-        return jsonify({'token':token}), 200        
-    return jsonify({'message':'password does not match !'}),401
-
-@mod.route('/parcels', methods=['POST'])
+@parcel.route('/parcels', methods=['POST'])
 @jwt_required
 def make_order():
     """ This function enables a user to make a parcel delivery order """
@@ -75,7 +35,7 @@ def make_order():
     return jsonify({'message':'order placed successfully'}),201
 
 
-@mod.route('/parcels', methods=['GET'])
+@parcel.route('/parcels', methods=['GET'])
 @jwt_required
 def get_user_orders():
     """ This function enables a user to fetch his parcel delivery orders """
@@ -92,7 +52,7 @@ def get_user_orders():
     return jsonify({'placed orders':output}),200
 
 
-@mod.route('/parcels/<int:parcelId>/destination', methods=['PUT'])
+@parcel.route('/parcels/<int:parcelId>/destination', methods=['PUT'])
 @jwt_required
 def change_destination(parcelId):
     """ This function enables a user to change the destination of a parcel delivery order """
@@ -107,7 +67,7 @@ def change_destination(parcelId):
         return jsonify({'message':'Failed to update parcel delivery order destination'}),400
     return jsonify({'message':'destination of parcel delivery order changed'}),200
 
-@mod.route('/parcels/<int:parcelId>/status', methods=['PUT'])
+@parcel.route('/parcels/<int:parcelId>/status', methods=['PUT'])
 @jwt_required
 def status(parcelId):
     """ This function enables an admin user to change the status a parcel delivery order """
@@ -123,7 +83,7 @@ def status(parcelId):
 
     return jsonify({'message':'status of parcel delivery order changed'}),200
     
-@mod.route('/parcels/<int:parcelId>/presentLocation',methods=['PUT'])
+@parcel.route('/parcels/<int:parcelId>/presentLocation',methods=['PUT'])
 @jwt_required
 def change_present_location(parcelId):
     """ This function enables a user to change the present location of a parcel delivery order """
@@ -140,7 +100,7 @@ def change_present_location(parcelId):
     return jsonify({'message':'present location of parcel delivery order changed'}),200
 
 
-@mod.route('/admin/parcels', methods=['GET'])
+@parcel.route('/admin/parcels', methods=['GET'])
 @jwt_required
 def get_all_user_orders():
     """ This function enables an admin user to get all parcel delivery orders in the system """
@@ -156,15 +116,5 @@ def get_all_user_orders():
     for order in placed_orders:
         output.append(order)
     return jsonify({'placed orders':output}),200
-
-@mod.route('/promote/<username>',methods=['PUT'])
-def promote_user(username):
-    """ This function promotes a user to admin """
-    user = user_object.user(username)
-    if user is None:
-        return jsonify({'message':'user promotion failed'}),400
-    result_set =user_object.promoter(username)
-    if result_set is not None:
-        return jsonify({'message':username + ' promoted to admin'}),200
     
     
