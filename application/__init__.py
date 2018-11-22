6f9673edfe786e9bc546cad0a604109106dadcce
nyabongoedgar <nyabongoedgar@gmail.com>
2018-11-22 13:56:29 +0300

    change configuration based on dev environment

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 from application.api.views import mod
 app.register_blueprint(api.views.mod)
 
-app.config.from_object('config.BaseConfig')
+app.config.from_object('config.ProductionConfig')
 
 
